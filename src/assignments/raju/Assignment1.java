319f657c1f3ed862aeb42302e7c1f1b9a9b73953
Automation Test Planet <automationtesttrainings@gmail.com>
2026-01-27 09:38:41 +0530

    Assignment1 code review changes

--- src/assignments/raju/Assignment1.java
+++ src/assignments/raju/Assignment1.java
@@ -6,23 +6,16 @@ public class Assignment1 {
 	public static void reverseNumber(int number) {
 		System.out.println("Actual number: "+ number);
 		int revNum = 0;
-		
 		while(number>0) {
 			int remainder = number%10; // 5  // 5 // 1
-			
 			revNum = (revNum*10)+ remainder;  // 5 // 50+5 = 55  == 550+1 = 551
-			
 			number = number/10;   // 15  // 1	// 0
-			
 		}
-		
 		System.out.println("Reverse Number: "+ revNum);
-		
 	}
 
 	public static void main(String[] args) {
 		reverseNumber(155);
-		
 		reverseNumber(123);
 
 	}
