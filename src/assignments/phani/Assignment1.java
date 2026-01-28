c8ed6b54ed2000716593f8b681b6eef66dbdc287
Phani <phani.palwadi@gmail.com>
2026-01-28 23:14:47 +0530

    Code to print Amstrong Numbers between 100 to 1000

--- src/assignments/phani/Assignment1.java
+++ src/assignments/phani/Assignment1.java
@@ -6,9 +6,28 @@ public class Assignment1 {
 		// Ex: 100  to 1000 -> 153, 370, 371, 407
 		 
 
-	public static void main(String[] args) {
+
+		public static void main(String[] args) {
+			System.out.println("armstrong numbers between 100 and 1000");
+		
+		for (int i =100;i<1000;i++)
+		{
+			int r , sum=0;
+			 int temp=i;
+			while(temp>0)
+			{
+			r=temp % 10;
+			sum = sum + (r*r*r);
+			temp=temp/10;
+			}
+			if(i==sum)
+				System.out.println(i);
+		}
+		
 		
 
-	}
+			    }
+
+	
 
 }
