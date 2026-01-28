d908f2e289c6f0c792c509bca7f32588f2de37fb
Saritha <sarithagowda0210@gmail.com>
2026-01-28 21:27:39 +0530

    First Commit

--- src/assignments/saritha/Assignment1.java
+++ src/assignments/saritha/Assignment1.java
@@ -6,7 +6,20 @@ public class Assignment1 {
 	// int[] intArr = {5,3,1,4,2}  -> {1,2,3,4,5}
 
 	public static void main(String[] args) {
-		
+		 
+		int arr[] = { 5,3,1,4,2 };
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+				
+				int temp = 0;
+				
+                if (arr[j] < arr[i]) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+	        }
+			System.out.print(arr[i] + " ");
 
 	}
 
