e56e40302cd9eb753199e12f35a660f40be61f0c
karthik <chinnajanu456@gmail.com>
2026-02-02 00:36:26 +0530

    assignment1 by karthik

--- src/assignments/karthik/Assignment1.java
+++ src/assignments/karthik/Assignment1.java
@@ -6,10 +6,27 @@ public class Assignment1 {
 		// Ex: 1 to 10  -> 1,2,3,5,7
 		// Ex: 1 to 20  -> 1,2,3,5,7,11,13,17,19
 
-
+	public static void primeNumbers(int start, int end) {
+		
+	
+		for(int i= start; i<= end; i++) {
+			int count = 0; 
+			
+			for(int j = 1; j<=i; j++) {
+				if(i % j == 0) {
+					count++;
+				}
+			}
+			if(count == 2) {
+				System.out.println(i+ " ");
+			}
+		}
+	}
 	public static void main(String[] args) {
 		
-
+			int start = 1;
+			int end = 100;
+			primeNumbers(start,end);
 	}
 
 }
